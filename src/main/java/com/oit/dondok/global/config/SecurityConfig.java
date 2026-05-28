837b2b21f26cff31cace0bac7b61b98be52ccf10
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-05-28 11:32:10 +0900

    feat: Security config 세팅 {#33}

---
+++ src/main/java/com/oit/dondok/global/config/SecurityConfig.java
@@ -0,0 +1,93 @@
+package com.oit.dondok.global.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+public class SecurityConfig {
+
+  private static final List<String> ALLOWED_ORIGINS =
+      List.of("http://localhost:3000", "https://dondok-fe.vercel.app");
+  private static final List<String> ALLOWED_METHODS =
+      List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS");
+  private static final List<String> ALLOWED_HEADERS = List.of("*");
+  private static final List<String> EXPOSED_HEADERS = List.of("Authorization");
+
+  private static final String[] POST_PERMIT_ALL_PATTERNS = {
+    "/api/auth/signup", "/api/auth/login", "/api/auth/refresh"
+  };
+
+  private static final String[] PERMIT_ALL_PATTERNS = {
+    //      "/swagger-ui.html",
+    //      "/swagger-ui/**",
+    //      "/v3/api-docs/**",
+    //      "/actuator/health"
+  };
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    disableSecurityBasic(http);
+    configureCors(http);
+    configureSessionManagement(http);
+    configureAuthorization(http);
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  // 기본 인증 끄기
+  private void disableSecurityBasic(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .formLogin(formLogin -> formLogin.disable())
+        .httpBasic(httpBasic -> httpBasic.disable());
+  }
+
+  // 프론트와 백엔드가 다른 주소에서 동작할 때 요청 허용
+  private void configureCors(HttpSecurity http) throws Exception {
+    http.cors(
+        cors ->
+            cors.configurationSource(
+                request -> {
+                  CorsConfiguration config = new CorsConfiguration();
+                  config.setAllowedOrigins(ALLOWED_ORIGINS);
+                  config.setAllowedMethods(ALLOWED_METHODS);
+                  config.setAllowedHeaders(ALLOWED_HEADERS);
+                  config.setExposedHeaders(EXPOSED_HEADERS);
+                  config.setAllowCredentials(true);
+                  return config;
+                }));
+  }
+
+  // 세션 허용 x
+  private void configureSessionManagement(HttpSecurity http) throws Exception {
+    http.sessionManagement(
+        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+  }
+
+  // 열 API와 닫을 API 구분
+  private void configureAuthorization(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+        auth ->
+            auth.requestMatchers(CorsUtils::isPreFlightRequest)
+                .permitAll()
+                .requestMatchers(HttpMethod.POST, POST_PERMIT_ALL_PATTERNS)
+                .permitAll()
+                .requestMatchers(PERMIT_ALL_PATTERNS)
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+  }
+}
