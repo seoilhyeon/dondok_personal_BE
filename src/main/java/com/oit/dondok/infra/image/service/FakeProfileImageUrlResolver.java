34a9ea3113a8b598cba61132ac0c87ebb61f74fe
seoilhyeon <ilhyeon0326@gmail.com>
2026-06-01 19:08:50 +0900

    feat: fake ProfileImageUrlResolver 추가 (#90)

---
+++ src/main/java/com/oit/dondok/infra/image/service/FakeProfileImageUrlResolver.java
@@ -0,0 +1,15 @@
+package com.oit.dondok.infra.image.service;
+
+import com.oit.dondok.domain.member.port.ProfileImageUrlResolver;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile({"local", "dev", "test", "integration"})
+public class FakeProfileImageUrlResolver implements ProfileImageUrlResolver {
+
+  @Override
+  public String resolveProfileImageUrl(String profileImageS3Key) {
+    return null;
+  }
+}
