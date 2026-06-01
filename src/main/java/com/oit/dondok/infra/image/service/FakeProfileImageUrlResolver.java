3299b800aa17dcdc0c435f47b2823ed1c7d2b690
seoilhyeon <ilhyeon0326@gmail.com>
2026-06-01 19:16:29 +0900

    chore: TODO 주석 추가 (#90)

--- src/main/java/com/oit/dondok/infra/image/service/FakeProfileImageUrlResolver.java
+++ src/main/java/com/oit/dondok/infra/image/service/FakeProfileImageUrlResolver.java
@@ -10,6 +10,7 @@ public class FakeProfileImageUrlResolver implements ProfileImageUrlResolver {
 
   @Override
   public String resolveProfileImageUrl(String profileImageS3Key) {
+    // TODO(S3): Replace this non-prod fallback with an actual profile image URL resolver.
     return null;
   }
 }
