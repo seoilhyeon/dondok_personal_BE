4c2d2b0e80de15ba8cd73f6f36bac2095c00ee58
jeonseong0120 <jeonseong922@gmail.com>
2026-06-01 11:19:35 +0900

    fix: CrewPointPort Fake 구현체 프로파일 분리 및 빈 충돌 방지

--- src/main/java/com/oit/dondok/domain/crew/port/FakeCrewPointPort.java
+++ src/main/java/com/oit/dondok/domain/crew/port/FakeCrewPointPort.java
@@ -1,11 +1,11 @@
 package com.oit.dondok.domain.crew.port;
 
 import com.oit.dondok.domain.crew.entity.CrewParticipant;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Primary
 @Component
+@Profile({"local", "dev", "integration"})
 public class FakeCrewPointPort implements CrewPointPort {
 
   // TODO: 실제 포인트 락 로직 구현 필요 (PointAccount 잔액 차감 + PointHistory append)
