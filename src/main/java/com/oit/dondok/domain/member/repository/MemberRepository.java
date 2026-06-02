432bdaa8215e3db6f356f1ad5def3540e2e3b657
seoilhyeon <ilhyeon0326@gmail.com>
2026-06-02 13:18:48 +0900

    feat: 회원 프로필 업데이트 기능 추가 (#94)

--- src/main/java/com/oit/dondok/domain/member/repository/MemberRepository.java
+++ src/main/java/com/oit/dondok/domain/member/repository/MemberRepository.java
@@ -14,4 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   boolean existsByEmail(String email);
 
   boolean existsByNickname(String nickname);
+
+  boolean existsByNicknameAndUuidNot(String nickname, UUID uuid);
 }
--- src/test/java/com/oit/dondok/domain/member/service/MemberProfileServiceTest.java
+++ src/test/java/com/oit/dondok/domain/member/service/MemberProfileServiceTest.java
@@ -5,13 +5,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
+import com.oit.dondok.domain.member.dto.request.UpdateProfileRequest;
 import com.oit.dondok.domain.member.dto.response.ProfileResponse;
+import com.oit.dondok.domain.member.dto.response.ProfileUpdateResponse;
+import com.oit.dondok.domain.member.entity.Member;
 import com.oit.dondok.domain.member.entity.MemberStatus;
 import com.oit.dondok.domain.member.exception.MemberErrorCode;
 import com.oit.dondok.domain.member.port.ProfileImageUrlResolver;
 import com.oit.dondok.domain.member.repository.MemberProfileProjection;
 import com.oit.dondok.domain.member.repository.MemberProfileQueryRepository;
+import com.oit.dondok.domain.member.repository.MemberRepository;
 import com.oit.dondok.global.exception.CustomException;
+import com.oit.dondok.global.exception.GlobalErrorCode;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
@@ -20,10 +26,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.openapitools.jackson.nullable.JsonNullable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class MemberProfileServiceTest {
 
+  @Mock private MemberRepository memberRepository;
   @Mock private MemberProfileQueryRepository memberProfileQueryRepository;
   @Mock private ProfileImageUrlResolver profileImageUrlResolver;
 
@@ -121,6 +130,104 @@ class MemberProfileServiceTest {
         .isInstanceOf(CustomException.class)
         .extracting("errorCode")
         .isEqualTo(MemberErrorCode.MEMBER_NOT_FOUND);
+    then(profileImageUrlResolver).shouldHaveNoInteractions();
+  }
+
+  @Test
+  void updateProfileUpdatesIncludedFieldsAndReturnsProfileUpdateResponse() {
+    UUID memberUuid = UUID.fromString("018f4fd2-6d7a-7a41-9f58-6d07f5c3c904");
+    Member member = Member.create("member@example.com", "password-hash", "기존닉네임");
+    ReflectionTestUtils.setField(member, "uuid", memberUuid);
+    ReflectionTestUtils.setField(member, "updatedAt", LocalDateTime.of(2026, 6, 2, 11, 0));
+    String profileImageS3Key = "profile/new-image.png";
+    String profileImageUrl = "https://cdn.example.com/profile/new-image.png";
+    UpdateProfileRequest request =
+        new UpdateProfileRequest(
+            JsonNullable.of("새닉네임"), JsonNullable.of(profileImageS3Key), JsonNullable.of(null));
+    given(memberRepository.findByUuid(memberUuid)).willReturn(Optional.of(member));
+    given(memberRepository.existsByNicknameAndUuidNot("새닉네임", memberUuid)).willReturn(false);
+    given(memberRepository.saveAndFlush(member)).willReturn(member);
+    given(profileImageUrlResolver.resolveProfileImageUrl(profileImageS3Key))
+        .willReturn(profileImageUrl);
+
+    ProfileUpdateResponse response = memberProfileService.updateProfile(memberUuid, request);
+
+    assertThat(member.getNickname()).isEqualTo("새닉네임");
+    assertThat(member.getProfileImageS3Key()).isEqualTo(profileImageS3Key);
+    assertThat(member.getStatusMessage()).isNull();
+    assertThat(response.memberUuid()).isEqualTo(memberUuid);
+    assertThat(response.email()).isEqualTo("member@example.com");
+    assertThat(response.nickname()).isEqualTo("새닉네임");
+    assertThat(response.profileImageUrl()).isEqualTo(profileImageUrl);
+    assertThat(response.statusMessage()).isNull();
+    assertThat(response.updatedAt()).isEqualTo(OffsetDateTime.parse("2026-06-02T11:00:00+09:00"));
+    then(memberRepository).should().saveAndFlush(member);
+  }
+
+  @Test
+  void updateProfileKeepsOmittedFieldsAndRemovesProfileImageWhenExplicitNullIsIncluded() {
+    UUID memberUuid = UUID.fromString("018f4fd2-6d7a-7a41-9f58-6d07f5c3c905");
+    Member member = Member.create("member@example.com", "password-hash", "기존닉네임");
+    ReflectionTestUtils.setField(member, "uuid", memberUuid);
+    ReflectionTestUtils.setField(member, "updatedAt", LocalDateTime.of(2026, 6, 2, 11, 10));
+    member.updateProfile("기존닉네임", "profile/old-image.png", "기존 상태 메시지");
+    UpdateProfileRequest request =
+        new UpdateProfileRequest(null, JsonNullable.of(null), JsonNullable.undefined());
+    given(memberRepository.findByUuid(memberUuid)).willReturn(Optional.of(member));
+    given(memberRepository.saveAndFlush(member)).willReturn(member);
+
+    ProfileUpdateResponse response = memberProfileService.updateProfile(memberUuid, request);
+
+    assertThat(member.getNickname()).isEqualTo("기존닉네임");
+    assertThat(member.getProfileImageS3Key()).isNull();
+    assertThat(member.getStatusMessage()).isEqualTo("기존 상태 메시지");
+    assertThat(response.profileImageUrl()).isNull();
+    assertThat(response.statusMessage()).isEqualTo("기존 상태 메시지");
+    then(profileImageUrlResolver).shouldHaveNoInteractions();
+  }
+
+  @Test
+  void updateProfileThrowsInvalidInputWhenNoFieldIsIncluded() {
+    UUID memberUuid = UUID.randomUUID();
+    UpdateProfileRequest request = new UpdateProfileRequest(null, null, null);
+
+    assertThatThrownBy(() -> memberProfileService.updateProfile(memberUuid, request))
+        .isInstanceOf(CustomException.class)
+        .extracting("errorCode")
+        .isEqualTo(GlobalErrorCode.INVALID_INPUT);
+    then(memberRepository).shouldHaveNoInteractions();
+    then(profileImageUrlResolver).shouldHaveNoInteractions();
+  }
+
+  @Test
+  void updateProfileThrowsWhenMemberIsMissing() {
+    UUID memberUuid = UUID.randomUUID();
+    UpdateProfileRequest request = new UpdateProfileRequest(JsonNullable.of("새닉네임"), null, null);
+    given(memberRepository.findByUuid(memberUuid)).willReturn(Optional.empty());
+
+    assertThatThrownBy(() -> memberProfileService.updateProfile(memberUuid, request))
+        .isInstanceOf(CustomException.class)
+        .extracting("errorCode")
+        .isEqualTo(MemberErrorCode.MEMBER_NOT_FOUND);
+    then(memberRepository).should().findByUuid(memberUuid);
+    then(memberRepository).shouldHaveNoMoreInteractions();
+    then(profileImageUrlResolver).shouldHaveNoInteractions();
+  }
+
+  @Test
+  void updateProfileThrowsWhenNicknameIsAlreadyUsedByAnotherMember() {
+    UUID memberUuid = UUID.fromString("018f4fd2-6d7a-7a41-9f58-6d07f5c3c906");
+    Member member = Member.create("member@example.com", "password-hash", "기존닉네임");
+    UpdateProfileRequest request = new UpdateProfileRequest(JsonNullable.of(" 새닉네임 "), null, null);
+    given(memberRepository.findByUuid(memberUuid)).willReturn(Optional.of(member));
+    given(memberRepository.existsByNicknameAndUuidNot("새닉네임", memberUuid)).willReturn(true);
+
+    assertThatThrownBy(() -> memberProfileService.updateProfile(memberUuid, request))
+        .isInstanceOf(CustomException.class)
+        .extracting("errorCode")
+        .isEqualTo(MemberErrorCode.NICKNAME_ALREADY_EXISTS);
+    then(memberRepository).should().existsByNicknameAndUuidNot("새닉네임", memberUuid);
+    then(memberRepository).shouldHaveNoMoreInteractions();
     then(profileImageUrlResolver).shouldHaveNoInteractions();
   }
 }
