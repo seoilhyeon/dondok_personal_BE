381a6ae216b49ed1ff45df70ea2a437aad1db55c
Changhyun Moon <67505983+changhyunmoon@users.noreply.github.com>
2026-06-05 22:44:21 +0900

    Fix: 기본 보안 사용자 자동 생성 방지 {#135} (#136)

--- src/main/java/com/oit/dondok/DondokApplication.java
+++ src/main/java/com/oit/dondok/DondokApplication.java
@@ -2,10 +2,11 @@ package com.oit.dondok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class DondokApplication {
 
   public static void main(String[] args) {
