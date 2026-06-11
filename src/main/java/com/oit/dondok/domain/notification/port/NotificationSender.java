4b2e891ae8520e3a80a737d1cc09909a420eeda8
김한비 <soleil666111@gmail.com>
2026-06-11 10:25:04 +0900

    Revert "feat: FCM firebase-admin SDK 설정 및 NotificationSender 추상화 (#187)"
    This reverts commit 703b5689d0c0fb8cb52caf9b0184db174ac46e90.

--- src/main/java/com/oit/dondok/domain/notification/port/NotificationSender.java
+++
@@ -1,8 +0,0 @@
-package com.oit.dondok.domain.notification.port;
-
-import com.oit.dondok.domain.member.entity.Member;
-
-public interface NotificationSender {
-
-  void send(Member member, NotificationPayload payload);
-}
--- src/main/java/com/oit/dondok/infra/fcm/config/FcmProperties.java
+++
@@ -1,6 +0,0 @@
-package com.oit.dondok.infra.fcm.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "app.firebase")
-public record FcmProperties(String credentialsPath) {}
