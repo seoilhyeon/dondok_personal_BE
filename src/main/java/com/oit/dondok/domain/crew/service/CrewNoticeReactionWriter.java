142c415a2b2667ae1425c885d22fefd9d897859c
김한비 <soleil666111@gmail.com>
2026-06-12 10:55:49 +0900

    fix: 코드래빗 댓글 1개 반영 (#226)

--- src/main/java/com/oit/dondok/domain/crew/service/CrewNoticeReactionWriter.java
+++ src/main/java/com/oit/dondok/domain/crew/service/CrewNoticeReactionWriter.java
@@ -3,6 +3,7 @@ package com.oit.dondok.domain.crew.service;
 import com.oit.dondok.domain.crew.entity.CrewNoticeReaction;
 import com.oit.dondok.domain.crew.repository.CrewNoticeReactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
@@ -12,13 +13,26 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 class CrewNoticeReactionWriter {
 
+  private static final String UK_REACTION = "uk_crew_notice_reaction_notice_member_type";
+
   private final CrewNoticeReactionRepository crewNoticeReactionRepository;
 
   @Transactional(propagation = Propagation.REQUIRES_NEW)
   public void saveIgnoreDuplicate(CrewNoticeReaction reaction) {
     try {
       crewNoticeReactionRepository.saveAndFlush(reaction);
-    } catch (DataIntegrityViolationException ignored) {
+    } catch (DataIntegrityViolationException exception) {
+      Throwable cause = exception;
+      while (cause != null) {
+        if (cause instanceof ConstraintViolationException constraintException) {
+          if (UK_REACTION.equalsIgnoreCase(constraintException.getConstraintName())) {
+            return;
+          }
+          break;
+        }
+        cause = cause.getCause();
+      }
+      throw exception;
     }
   }
 }
