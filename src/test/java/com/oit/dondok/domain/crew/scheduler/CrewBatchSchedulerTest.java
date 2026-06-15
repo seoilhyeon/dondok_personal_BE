6bf1d25605064dbd0460242daf70b786a7221b84
codingNoob12 <ilhyeon0326@gmail.com>
2026-06-15 15:03:45 +0900

    feat: CrewBatchScheduler에서 settlement 의존성 제거 (#267)

--- src/test/java/com/oit/dondok/domain/crew/scheduler/CrewBatchSchedulerTest.java
+++ src/test/java/com/oit/dondok/domain/crew/scheduler/CrewBatchSchedulerTest.java
@@ -1,23 +1,24 @@
 package com.oit.dondok.domain.crew.scheduler;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.doThrow;
 
 import com.oit.dondok.domain.crew.service.CrewActivationBatchService;
 import com.oit.dondok.domain.crew.service.PendingApplicationAutoRejectService;
-import com.oit.dondok.domain.settlement.service.SettlementBatchService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @ExtendWith(MockitoExtension.class)
 class CrewBatchSchedulerTest {
 
   @Mock private CrewActivationBatchService crewActivationBatchService;
   @Mock private PendingApplicationAutoRejectService pendingApplicationAutoRejectService;
-  @Mock private SettlementBatchService settlementBatchService;
 
   @InjectMocks private CrewBatchScheduler crewBatchScheduler;
 
@@ -27,10 +28,8 @@ class CrewBatchSchedulerTest {
 
     then(pendingApplicationAutoRejectService).should().rejectExpiredApplications();
     then(crewActivationBatchService).should().activateCrews();
-    then(settlementBatchService).should().runFinalSettlementBatch();
     then(pendingApplicationAutoRejectService).shouldHaveNoMoreInteractions();
     then(crewActivationBatchService).shouldHaveNoMoreInteractions();
-    then(settlementBatchService).shouldHaveNoMoreInteractions();
   }
 
   @Test
@@ -39,9 +38,30 @@ class CrewBatchSchedulerTest {
         .when(pendingApplicationAutoRejectService)
         .rejectExpiredApplications();
 
-    crewBatchScheduler.runDailyBatch();
+    assertThatCode(() -> crewBatchScheduler.runDailyBatch()).doesNotThrowAnyException();
 
     then(crewActivationBatchService).should().activateCrews();
-    then(settlementBatchService).should().runFinalSettlementBatch();
+  }
+
+  @Test
+  void runDailyBatchDoesNotPropagateActivationFailure() {
+    doThrow(new RuntimeException("activation error"))
+        .when(crewActivationBatchService)
+        .activateCrews();
+
+    assertThatCode(() -> crewBatchScheduler.runDailyBatch()).doesNotThrowAnyException();
+
+    then(pendingApplicationAutoRejectService).should().rejectExpiredApplications();
+    then(crewActivationBatchService).should().activateCrews();
+  }
+
+  @Test
+  void runDailyBatchUsesCrewLifecycleMidnightSchedule() throws NoSuchMethodException {
+    Scheduled scheduled =
+        CrewBatchScheduler.class.getMethod("runDailyBatch").getAnnotation(Scheduled.class);
+
+    assertThat(scheduled).isNotNull();
+    assertThat(scheduled.zone()).isEqualTo("Asia/Seoul");
+    assertThat(scheduled.cron()).isEqualTo("0 0 0 * * *");
   }
 }
