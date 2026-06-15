de5440165c606fd0ccc31ea4281d27153186ce31
codingNoob12 <ilhyeon0326@gmail.com>
2026-06-15 23:24:57 +0900

    feat: 최종 정산 미션 날짜 계산 로직 추가 (#276)

--- src/main/java/com/oit/dondok/domain/mission/repository/MissionScheduleDayRepository.java
+++ src/main/java/com/oit/dondok/domain/mission/repository/MissionScheduleDayRepository.java
@@ -1,10 +1,13 @@
 package com.oit.dondok.domain.mission.repository;
 
 import com.oit.dondok.domain.mission.entity.MissionScheduleDay;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissionScheduleDayRepository extends JpaRepository<MissionScheduleDay, Long> {
 
   // SPECIFIC_DAYS 크루의 미션 가능 요일(1=월 ~7=일, ISO-8601) 포함 여부
   boolean existsByMissionRuleIdAndDayOfWeek(Long missionRuleId, Integer dayOfWeek);
+
+  List<MissionScheduleDay> findByMissionRuleIdOrderByDayOfWeekAsc(Long missionRuleId);
 }
