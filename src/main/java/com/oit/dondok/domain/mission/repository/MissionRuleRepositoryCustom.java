280c12193b0dbcf5a65841ec81c72237e12a1b5a
codingNoob12 <ilhyeon0326@gmail.com>
2026-06-16 09:33:19 +0900

    fix: 크루 CLOSED시 finalized 생성 가능하도록 수정 (#276)

--- src/main/java/com/oit/dondok/domain/mission/repository/MissionRuleRepositoryCustom.java
+++ src/main/java/com/oit/dondok/domain/mission/repository/MissionRuleRepositoryCustom.java
@@ -1,15 +1,18 @@
 package com.oit.dondok.domain.mission.repository;
 
+import com.oit.dondok.domain.crew.entity.CrewStatus;
 import com.oit.dondok.domain.mission.entity.DailySettlementType;
 import com.oit.dondok.domain.mission.entity.MissionRule;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 public interface MissionRuleRepositoryCustom {
 
-  List<MissionRule> findActiveRulesForDailySettlement(
+  List<MissionRule> findRulesForDailySettlement(
       DailySettlementType dailySettlementType,
       LocalDateTime missionDateStartInclusive,
       LocalDateTime missionDateEndExclusive,
-      int dayOfWeek);
+      int dayOfWeek,
+      Collection<CrewStatus> crewStatuses);
 }
