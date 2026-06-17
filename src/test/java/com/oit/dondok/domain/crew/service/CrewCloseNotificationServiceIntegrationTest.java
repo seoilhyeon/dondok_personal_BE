5a920b4084f0c53d02e4aa3b7f2b5bb50ba62b48
김한비 <soleil666111@gmail.com>
2026-06-17 10:18:07 +0900

    test: 크루 종료 예정 알림 통합 테스트 추가 (#304)

--- src/test/java/com/oit/dondok/domain/crew/service/CrewCloseNotificationServiceIntegrationTest.java
+++ src/test/java/com/oit/dondok/domain/crew/service/CrewCloseNotificationServiceIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.oit.dondok.domain.crew.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
